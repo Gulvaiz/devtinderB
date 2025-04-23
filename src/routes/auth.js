f3acac0764d3464ea2fd33164eb6af7e1b90b54d
Gulvaiz <gulvaizalam12@gmail.com>
2025-04-23 11:33:16 +0530

    asdfasdf

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -53,7 +53,7 @@ authRouter.post("/login", async (req, res) => {
             if(isPasswordValid){
 
                 const token = await user.getJWT()
-                res.cookie("token", token,  { expires: new Date(Date.now() + 8 * 3600000)})
+                res.cookie("token", token, { httpOnly:true, secure:true , expires: new Date(Date.now() + 8 * 3600000)})
                 
                 res.send(user)
             }
